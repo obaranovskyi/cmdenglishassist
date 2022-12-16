a2a62496b25912a495c605d99c89f5124dcae58c
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-16 07:09:35 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ echo 'Installing cmdenglishassist'
 mkdir -p ~/.config
 cd ~/.config
 python3 -m pip install -e git+https://github.com/obaranovskyi/cmdenglishassist.git#egg=cmdenglishassist
-cd src/cmdenglishassist
+cd ~/.config/src/cmdenglishassist
 pip install -r requirements.txt
